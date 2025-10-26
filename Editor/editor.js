768939243e4fc82b89ff5b2510755e51095de223
Richard Liu <richardadesigner@gmail.com>
2025-10-26 10:10:56 -0700

    update

--- Editor/editor.js
+++ Editor/editor.js
@@ -1,3 +1,12 @@
 document.getElementById("input").addEventListener("input", function () {
     document.getElementById("display").srcdoc = document.getElementById("input").value
-})
+});
+let urlInput = new URLSearchParams(window.location.search)
+fetch("source/" + urlInput.get('file') + ".html")
+    .then(function (response) {
+        return response.text()
+    })
+    .then(function (html) {
+        document.getElementById("input").value = html;
+        document.getElementById("display").srcdoc = document.getElementById("input").value
+    })
